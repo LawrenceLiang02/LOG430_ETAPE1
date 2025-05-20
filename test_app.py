afb70d79488f76a078a8797ab84e46ca106d708e
LawrenceLiang02 <lawrencekaiwei@gmail.com>
2025-05-20 18:26:08 -0400

    linter

--- test_app.py
+++ test_app.py
@@ -1,14 +1,16 @@
+# pylint: disable=redefined-outer-name
+
 """Tests"""
 import pytest
 from app import create_app
 
 @pytest.fixture
-def client():
+def test_client():
     """Test client"""
     app = create_app()
     app.config["TESTING"] = True
-    with app.test_client() as test_client:
-        yield test_client
+    with app.test_client() as client:
+        yield client
 
 def test_root_status_code(test_client):
     """Test status code"""
