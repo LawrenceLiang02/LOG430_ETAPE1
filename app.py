e1c882cbc395247f329a1bbe0a7d0105e2c175b4
LawrenceLiang02 <lawrencekaiwei@gmail.com>
2025-05-20 18:20:20 -0400

    linter

--- app.py
+++ app.py
@@ -1,22 +1,16 @@
-"""
-    This module does something
-"""
+"""This module does something"""
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    """
-        Test method
-    """
+    """Test method"""
     return "Hello, World!"
 
 
 def create_app():
-    """
-        Method for tests
-    """
+    """Method for tests"""
     return app
 
 if __name__ == "__main__":
--- test_app.py
+++ test_app.py
@@ -1,29 +1,21 @@
-"""
-    Tests
-"""
+"""Tests"""
 import pytest
 from app import create_app
 
 @pytest.fixture
 def client():
-    """
-        Test client
-    """
+    """Test client"""
     app = create_app()
     app.config["TESTING"] = True
-    with app.test_client() as client:
-        yield client
+    with app.test_client() as test_client:
+        yield test_client
 
-def test_root_status_code(client):
-    """
-        Test status code
-    """
-    response = client.get("/")
+def test_root_status_code(test_client):
+    """Test status code"""
+    response = test_client.get("/")
     assert response.status_code == 200
 
-def test_root_content(client):
-    """
-        Test content
-    """
-    response = client.get("/")
+def test_root_content(test_client):
+    """Test content"""
+    response = test_client.get("/")
     assert b"Hello, World!" in response.data
