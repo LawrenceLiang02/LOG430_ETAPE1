475f26b2fca115fd4f5a33578529ed882fede21a
LawrenceLiang02 <lawrencekaiwei@gmail.com>
2025-05-26 02:02:43 -0400

    remove old tests

--- test_app.py
+++ test_app.py
@@ -2,32 +2,3 @@
 
 """Tests"""
 import pytest
-# from app import create_app
-
-# @pytest.fixture
-# def test_client():
-#     """Test client"""
-#     app = create_app()
-#     app.config["TESTING"] = True
-#     with app.test_client() as client:
-#         yield client
-
-# def test_root_status_code(test_client):
-#     """Test status code"""
-#     response = test_client.get("/")
-#     assert response.status_code == 200
-
-# def test_root_content(test_client):
-#     """Test content"""
-#     response = test_client.get("/")
-#     assert b"Hello, World!" in response.data
-
-from app import get_greeting
-
-def test_greeting_equals_hello_world():
-    """This method tests the get greetings method"""
-    assert get_greeting() == "Hello, world!"
-
-def test_greeting_is_not_empty():
-    """This tests that get greetings is not empty"""
-    assert get_greeting() != ""
