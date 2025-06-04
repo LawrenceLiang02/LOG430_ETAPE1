b6d6c3634a1c14fea4ddcbd315a81c9a3f1949ff
LawrenceLiang02 <lawrencekaiwei@gmail.com>
2025-06-04 16:38:38 -0400

    meet pylint requirement

--- tests/service_layer/test_location_service.py
+++ tests/service_layer/test_location_service.py
@@ -1,3 +1,4 @@
+"""Test module for location service"""
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -10,12 +11,14 @@ TestingSessionLocal = sessionmaker(bind=engine)
 
 @pytest.fixture(scope="module", autouse=True)
 def setup_database():
+    """Function to et up database"""
     Base.metadata.create_all(engine)
     yield
     Base.metadata.drop_all(engine)
 
 @pytest.fixture(scope="function", autouse=True)
 def seed_data():
+    """default seed data"""
     session = TestingSessionLocal()
     session.query(Location).delete()
     session.commit()
@@ -31,18 +34,22 @@ def seed_data():
 
 @pytest.fixture(autouse=True)
 def override_session(monkeypatch):
+    """Method to override session"""
     monkeypatch.setattr("service_layer.location_repository.SessionLocal", TestingSessionLocal)
 
 def test_get_all_locations():
+    """Unit test to get all lcoations"""
     locations = get_all_locations()
     assert len(locations) == 3
     assert [loc.name for loc in locations] == ["Store A", "Warehouse Z", "store b"]
 
 def test_get_location_by_name_case_insensitive():
+    """Test to get all lcoation by name"""
     loc = get_location_by_name("STORE A")
     assert loc is not None
     assert loc.name == "Store A"
 
 def test_get_location_by_name_not_found():
+    """Test to get all locations but returns none"""
     loc = get_location_by_name("NotExist")
     assert loc is None
